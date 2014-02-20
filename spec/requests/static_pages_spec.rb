4d57e69266006c90acf76292d5e285d1f717b860
Nick Hunter <hunter.497@gmail.com>
2014-02-20 07:38:47 -0500

    Finish static pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,22 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Twitter Clone')
     end
+
+  	it "should have the right title" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Ruby on Rails Tutorial Twitter Clone | Home")
+  	end
   end
 
   describe "Help page" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
+  	end
+
+  	it "should have the right title" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Ruby on Rails Tutorial Twitter Clone | Help")
   	end
   end
 
@@ -21,6 +31,10 @@ describe "StaticPages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
-  end
 
+  	it "should have the right title" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("Ruby on Rails Tutorial Twitter Clone | About")
+  	end
+  end
 end
